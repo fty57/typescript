94bb49f1e705dfce25cabd9344a4c1b4d3ed0393
fty57 <fty.57@alu.ufc.br>
2023-05-29 09:40:07 -0300

    437. Tipo Enum

---
+++ src/A0009-tipo-enum/A0009-tipo-enum.ts
@@ -0,0 +1,22 @@
+enum Cores {
+  VERMELHO = 10, // 0
+  AZUL = 100, // 1
+  AMARELO = 200, // 2
+}
+
+enum Cores {
+  ROXO = 'Roxo',
+  VERDE = 201,
+  ROSA,
+}
+
+console.log(Cores);
+/* console.log(Cores.VERMELHO);
+console.log(Cores[10]);
+console.log(Cores.ROXO); */
+
+function escolhaACor(cor: number): void {
+  console.log(Cores[cor]);
+}
+
+escolhaACor(201);
